2e88b16f331458769805d5dc6fe0228513760934
RaviGhaghada <ravighaghada28@gmail.com>
2020-06-10 14:11:27 +0100

    Can add new tasks via a form

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Task from './Task.js';
-
 import { Tasks } from '../api/tasks.js';
 import { withTracker } from 'meteor/react-meteor-data';
 
@@ -12,11 +12,27 @@ class App extends Component {
     ));
   }
 
+  handleSubmit(event) {
+    event.preventDefault()
+
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current datetime
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input type="text" ref="textInput" placeholder="Type to add new tasks" />
+          </form>
         </header>
 
         <ul>
--- server/main.js
+++ server/main.js
@@ -4,4 +4,5 @@ import '../imports/api/tasks.js'
 
 Meteor.startup(() => {
   // code to run on server at startup
+
 });
