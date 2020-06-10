0da8cd8e7d2b72b778c41f013a4233fc3050bd64
RaviGhaghada <ravighaghada28@gmail.com>
2020-06-10 14:33:57 +0100

    Added hideCompleted checkbox

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -6,8 +6,22 @@ import { withTracker } from 'meteor/react-meteor-data';
 
 // App component - represents the whole app
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false
+    };
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -25,11 +39,23 @@ class App extends Component {
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <label className="hide-completed">
+            <input type="checkbox" readOnly checked={this.state.hideCompleted} onClick={this.toggleHideCompleted.bind(this)} />
+            Hide Completed Tasks
+          </label>
+
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
             <input type="text" ref="textInput" placeholder="Type to add new tasks" />
           </form>
