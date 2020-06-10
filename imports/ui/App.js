9faf64c3e7c20ee5d1fbc8ef9dabcbe72d49ff30
RaviGhaghada <ravighaghada28@gmail.com>
2020-06-10 14:12:46 +0100

    Tasks get sorted by date

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -45,6 +45,6 @@ class App extends Component {
 
 export default withTracker(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 })(App);
